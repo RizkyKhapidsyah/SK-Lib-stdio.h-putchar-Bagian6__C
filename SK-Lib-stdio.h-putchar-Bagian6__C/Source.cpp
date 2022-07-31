959d25965c050418af85a77e4342733c4cd1ca77
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-07-31 09:43:06 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-putchar-Bagian6__C/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+    Source by AlphaCodingSkills (https://www.alphacodingskills.com)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+        putchar(ch);
+    }
+        
+    _getch();
+    return 0;
+}
+
